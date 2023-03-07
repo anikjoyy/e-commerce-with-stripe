2dfb34ecdc5392532370df7ee1353e2c7b234f04
anikjoyy <anikjoy.cse@gmail.com>
2023-03-07 22:46:05 +0600

    created main section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import MainSection from './components/MainSection/MainSection';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Hero />
+      <MainSection />
     </div>
   );
 }
